e6436b976c0e481e976c836270cbdac042f8a389
CoderTim <tim@timz.me>
2021-09-08 13:15:41 -0700

    Change default input plugin to PJ64_NRage.dll

--- NetplayInputPlugin/settings.cpp
+++ NetplayInputPlugin/settings.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 settings::settings(const string& settings_path) : settings_path(settings_path) {
     wchar_t plugin_dll[MAX_PATH];
-    GetPrivateProfileString(L"plugin", L"dll", L"Jabo_DInput.dll", plugin_dll, MAX_PATH, utf8_to_wstring(settings_path).c_str());
+    GetPrivateProfileString(L"plugin", L"dll", L"PJ64_NRage.dll", plugin_dll, MAX_PATH, utf8_to_wstring(settings_path).c_str());
     this->plugin_dll = wstring_to_utf8(plugin_dll);
 
     wchar_t name[256];
